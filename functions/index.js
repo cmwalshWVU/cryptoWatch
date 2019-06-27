df0f2c72d2a8d541a50bec8000a649337d8cc46b
Colin Walsh <colin.m.walsh@leidos.com>
2019-06-27 19:15:18 -0400

    Update firebase functions to create/update holdings from transactions

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,18 @@ const createNotification = (notification => {
         .then(doc =>   console.log('notification added', doc));
 })
 
+const createHolding = ((holding, uid) => {
+    return admin.firestore().collection('holdings').doc(uid).collection('holdings')
+        .add(holding)
+        .then(doc =>   console.log('holding added', doc));
+})
+
+const updateHolding = ((holding, uid) => {
+    return admin.firestore().collection('holdings').doc(uid).collection('holdings').doc(holding.token)
+        .update(holding)
+        .then(doc =>   console.log('holding updated', doc));
+})
+
 exports.projectCreated = functions.firestore
     .document('projects/{projectId}')
     .onCreate(doc => {
@@ -43,7 +55,33 @@ exports.userJoined = functions.auth.user()
 exports.transactionRecorded = functions.firestore
     .document('transactions/{userId}/transactions/{transactionId}')
     .onCreate((rec, context) => {
+        console.log('userID ', context.params.userId)
         const record = rec.data();
+        admin.firestore().collection('holdings').doc(context.params.userId).collection('holdings').doc(record.coin).get().then(doc => {
+            const isBuy = (record.isPurchase == "on" ? 1 : -1);
+
+            if (doc.data()) {
+                var newHoldings = doc.data().dollarHoldings + (isBuy * record.dollarAmount);
+                console.log('Updating holdings document for  ' + record.coin);
+                console.log('coin ', doc.data().coin);
+                console.log('Old Amount ', doc.data().dollarHoldings);
+                console.log('New Amount ', newHoldings);
+                admin.firestore().collection('holdings').doc(context.params.userId).collection('holdings').doc(record.coin).update({
+                    dollarHoldings: newHoldings
+                  });
+            }
+            else {
+                console.log('No holdings document exists for ' + record.coin + ' creating new doc');
+                var holdings = ((record.dollarAmount * isBuy) <= 0 ? 0 : record.dollarAmount);
+                admin.firestore().collection('holdings').doc(context.params.userId).collection('holdings').doc(record.coin).set({
+                    coin: record.coin,
+                    dollarHoldings: holdings
+                  });
+            }
+        }).catch(reason => {
+            console.log('error ', reason)
+        });
+
         const type = record.isPurchase;
         const purchase = (type == "on" ? "Purchase of" : "Sale of");
         const notification = {
@@ -52,6 +90,26 @@ exports.transactionRecorded = functions.firestore
             time: admin.firestore.FieldValue.serverTimestamp()
 
         };
-    return createNotification(notification);
         
+    return createNotification(notification);     
 });
+
+// exports.transactionRecorded = functions.firestore
+//     .document('transactions/{userId}/transactions/{transactionId}')
+//     .onCreate((rec, context) => {
+//         console.log('context ', context)
+//         const record = rec.data();
+//         const purchase = (record.isPurchase == "on" ? 1 : 1);
+//         const currentHolding = admin.firestore().collection('holdings').doc(context.auth.uid).collection('holdings').doc(rec.coin).get();
+//         if (currentHolding == null) {
+
+//         }
+//         const holding = {
+//             coin: record.coin,
+//             price: record.dollarAmount,
+//             time: admin.firestore.FieldValue.serverTimestamp()
+
+//         };
+//     return createHolding(holding, context.auth.uid);     
+// });
+
