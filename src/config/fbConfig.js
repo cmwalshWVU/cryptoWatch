1815a985da63aa2be8d1972b3e0a48e65f41d864
colin walsh <colinwalsh@colins-MacBook-Pro.local>
2019-12-08 22:07:21 -0500

    update dbconfig

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,15 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCSeuITZaO0J9X_tynA1Ja5jgDFZYlmfC0",
-    authDomain: "crypto-watch-dbf71.firebaseapp.com",
-    databaseURL: "https://crypto-watch-dbf71.firebaseio.com",
-    projectId: "crypto-watch-dbf71",
-    storageBucket: "crypto-watch-dbf71.appspot.com",
-    messagingSenderId: "671907255969",
-    appId: "1:671907255969:web:8c860d75a176bb10",
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyA5yLc27B1PKMYaCgWbj0NgvdUELWts4nQ",
+  authDomain: "crypto-watch-dbf71.firebaseapp.com",
+  databaseURL: "https://crypto-watch-dbf71.firebaseio.com",
+  projectId: "crypto-watch-dbf71",
+  storageBucket: "crypto-watch-dbf71.appspot.com",
+  messagingSenderId: "671907255969",
+  appId: "1:671907255969:web:8c860d75a176bb10"
+};
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({ timestampsInSnapshots: true });
