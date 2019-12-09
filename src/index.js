f1e333a9ed6516e71c639fb0ddf7f7b4849d7766
colin walsh <colinwalsh@colins-MacBook-Pro.local>
2019-12-09 10:00:55 -0500

    use env vars REACT_APP_

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ import './styles/root.css'
 ;
 
 var firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.SENDER_ID,
-    appId: process.env.APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const store = createStore(rootReducer, 
